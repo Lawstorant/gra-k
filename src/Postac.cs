471c8da33c23b8fa0b563108f7f19f9fea448a88
Tomasz Pakuła <forest10pl@gmail.com>
2020-04-28 23:30:27 +0200

    Wstępna praca nad klasą Postać

--- src/Postac.cs
+++ src/Postac.cs
@@ -2,6 +2,46 @@ namespace gra_k
 {
     public class Postac
     {
-        
+        protected uint zycie;
+        protected uint wytrzymalosc;
+        protected uint sila;
+        protected uint pancerz;
+        protected Cios[] listaCiosow;
+        protected bool obrona;
+
+        public Postac(uint zycie, uint wytrzymalosc, uint sila, uint pancerz)
+        {
+            this.zycie = zycie;
+            this.wytrzymalosc = wytrzymalosc;
+            this.sila = sila;
+            this.pancerz = pancerz;
+            this.obrona = false;
+            // this.listaCiosow = new Cios[];
+        }
+
+        public Cios[] pobierzCiosy()
+        {
+            return this.listaCiosow;
+        }
+
+        public void dodajCios(Cios dodawany)
+        {
+            // tutaj trzeba zrobić
+        }
+
+        public uint wykonajAtak(uint ciosIndex)
+        {
+            return this.sila * this.listaCiosow[ciosIndex].pobierzObrazenia();
+        }
+
+        public void pozycjaObronna()
+        {
+            this.obrona = true;
+        }
+
+        public void pobierzStatus()
+        {
+            // trzeba wypełnić
+        }
     }
 }
