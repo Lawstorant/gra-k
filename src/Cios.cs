cf183486b006d0933593da7483b91c6a7550cc55
Szakol <Szakol@users.noreply.github.com>
2020-04-29 21:20:14 +0200

    Update Cios.cs

--- src/Cios.cs
+++ src/Cios.cs
@@ -35,8 +35,7 @@ namespace gra_k
             this.obrazenia = uint.Parse(linie[1]);
             this.koszt = uint.Parse(linie[2]);
         }
-        // hej tomek
-        // witaj kacper
+
         public string pobierzNazwe()
         {
             return this.nazwa;
