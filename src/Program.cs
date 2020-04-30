06db12c48c9ab3e6f89e26a03887b80f6b7c5da6
Tomasz Pakuła <forest10pl@gmail.com>
2020-04-30 21:26:32 +0200

    Pozbycie się testu z maina

--- src/Program.cs
+++ src/Program.cs
@@ -6,11 +6,7 @@ namespace gra_k
     {
         static void Main(string[] args)
         {
-            Cwiczenie test1 = new Cwiczenie("cwiczenia/test.txt");
-            Przedmiot test2 = new Przedmiot("przedmioty/test.txt");
 
-            System.Console.WriteLine(test1.pobierzNazwe());
-            System.Console.WriteLine(test2.pobierzNazwe());
         }
     }
 }
