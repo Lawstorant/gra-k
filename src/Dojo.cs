dfdaea10d2dfc195aad7e4e394f888dc770d4e06
Tomasz Pakuła <forest10pl@gmail.com>
2020-05-01 13:20:32 +0200

    Dodano komentarze na temat rozwoju Dojo Będzie łatwiej, a nie trudniej

--- src/Dojo.cs
+++ src/Dojo.cs
@@ -6,7 +6,13 @@ namespace gra_k
         private Przedmiot[] listaPrzedmiotow;
         private Cwiczenie[] listaCwiczen;
 
-
+        public Dojo()
+        {
+            // policzyć pliki w folderach i zapisać ścieżki do tablicy
+            // na podstawie ilości zainicjować tablice o takim rozmiarze
+            // w pętli tworzyć poszczegołne ciosy / przedmioty / ćwiczenia na podstawie ścieżek
+            // (przekazywać ścieżki do konstruktorów, obiekty przypisywać do tablicy)
+        }
 
         public Cios[] pobierzCiosy()
         {
