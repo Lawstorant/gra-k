0e4e9cef7a280a2c8e538c7976a17ae3380ae5a6
Tomasz Pakuła <forest10pl@gmail.com>
2020-05-01 17:07:55 +0200

    Całkowicie rozwinięta klasa Postać Pozostawiam testy, bo i tak nie ma nic lepszego do wrzucenia do maina

--- src/Postac.cs
+++ src/Postac.cs
@@ -1,12 +1,22 @@
 using System.Collections.Generic;
+using System;
 
 namespace gra_k
 {
     public struct SilaObrony
     {
         public const double brak = 0;
-        public const double normalna = 0.4;
-        public const double mocna = 0.8;
+        public const double normalna = 0.6;
+        public const double mocna = 0.2;
+    }
+
+    public struct StatusPostaci
+    {
+        public uint zycie;
+        public uint wytrzymalosc;
+        public uint sila;
+        public uint pancerz;
+        public double obrona;
     }
 
     public class Postac
@@ -15,7 +25,7 @@ namespace gra_k
         protected uint wytrzymalosc;
         protected uint sila;
         protected uint pancerz;
-        // Lista ciosów jako generyczna lista dynamiczna, ułatwia dodawanie ciosóœ
+        // Lista ciosów jako generyczna lista dynamiczna, ułatwia dodawanie ciosów
         protected List<Cios> listaCiosow;
         protected double obrona;
 
@@ -50,21 +60,46 @@ namespace gra_k
 
         public uint wykonajAtak(uint ciosIndex)
         {
-            return this.sila * this.listaCiosow[(int)ciosIndex].pobierzObrazenia();
+            return this.sila * this.listaCiosow[(int) ciosIndex].pobierzObrazenia();
+        }
+
+        public void przyjmijObrazenia(uint obrazenia)
+        {
+            // COMEBAK: będzie potrzebny balans, może plik konfiguacyjny?
+            obrazenia -= this.pancerz;
+            obrazenia = (uint) Math.Ceiling ((this.obrona * (double)obrazenia));
+
+            if (obrazenia <= this.zycie)
+                this.zycie -= obrazenia;
+            else
+                zycie = 0;
         }
 
         public void pozycjaObronna(double moc)
         {
             this.obrona = moc;
-            this.wytrzymalosc -= (uint)(moc*5)/2;
+            if (moc == SilaObrony.normalna)
+                this.wytrzymalosc -= 1;
+            else
+                this.wytrzymalosc -= 2;
         }
 
-        public void pobierzStatus()
+        public StatusPostaci pobierzStatus()
         {
             // TODO: trzeba wypełnić
             // status jako struct? Ułatwiłoby to przesyłanie
             // do bufora wyświetlania
             // Tomek
+
+            var aktualnyStatus = new StatusPostaci();
+
+            aktualnyStatus.zycie = this.zycie;
+            aktualnyStatus.wytrzymalosc = this.wytrzymalosc;
+            aktualnyStatus.sila = this.sila;
+            aktualnyStatus.pancerz = this.pancerz;
+            aktualnyStatus.obrona = this.obrona;
+
+            return aktualnyStatus;
         }
     }
 }
