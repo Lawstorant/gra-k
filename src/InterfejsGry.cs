ca7bc12bf0fb3388d022b9ea22b0ec98ce50a9f8
Tomasz Pakuła <forest10pl@gmail.com>
2020-05-04 14:32:27 +0200

    Finalizacja UI Dojo Teraz zaczyna to fajnie wyglądać

--- src/InterfejsGry.cs
+++ src/InterfejsGry.cs
@@ -5,12 +5,36 @@ namespace gra_k
     public class InterfejsGry
     {
         private string[] opcjeDojo;
+        private string[] opcjeWalki;
+        private string[] opcjeGry;
 
         public InterfejsGry()
         {   
             // COMEBAK: Zrobić stringi w pliku i ich wczytywanie
             // stringi powinny być dla wszystkich okien
-            this.opcjeDojo = new string[3] {"Idz na silownie", "Poznaj nowe ciosy", "Kup sobie pancerz"};
+            this.opcjeDojo = new string[4] {"Idz na silownie", "Poznaj nowe ciosy", "Kup sobie pancerz", "Powrot"};
+            this.opcjeGry = new string[3] {"Idz na silownie", "Poznaj nowe ciosy", "Kup sobie pancerz"};
+            this.opcjeWalki = new string[3] {"Idz na silownie", "Poznaj nowe ciosy", "Kup sobie pancerz"};
+
+            // rzeczy windowsowe
+            try
+            {
+                // zmieniam wielkosć okna do rozmiaru gry
+                Console.SetWindowSize(120, 40);
+                Console.SetBufferSize(121, 41);
+                // próbuję zmienić kodowanie na en-US
+                //System.Diagnostics.Process.Start("CMD.exe", "chcp 437");
+            }
+            catch(Exception)
+            {
+    
+            }
+
+            // ustawiam domyślne kolory
+            Console.CursorVisible = false;            
+            //Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
         }
 
         public void pasekStatusu(
@@ -70,7 +94,6 @@ namespace gra_k
             ConsoleColor kolor = ConsoleColor.White;
             for (int i = 0; i < listaCiosow.Length; ++i)
             {
-                y += 2*i;
                 if(i == zaznaczonaOpcja)
                     kolor = ConsoleColor.Blue;
 
@@ -80,47 +103,82 @@ namespace gra_k
 
                 if(i == zaznaczonaOpcja)
                     kolor = ConsoleColor.White;
+
+                y += 2;
             }
         }
 
-        public void oknoCwiczen()
+        public void oknoCwiczen(Cwiczenie[] listaCwiczen, int zaznaczonaOpcja)
         {
             Wyswietlanie.okienko("Cwiczenia na silowni", 40, 3, 80, 30);
-        }
+            Wyswietlanie.pisz("Cwiczenie", ConsoleColor.White, 43, 6);
+            Wyswietlanie.pisz("koszt", ConsoleColor.White, 93, 6);
+            Wyswietlanie.pisz("zycie", ConsoleColor.White, 99, 6);
+            Wyswietlanie.pisz("stamina", ConsoleColor.White, 105, 6);
+            Wyswietlanie.pisz("sila", ConsoleColor.White, 114, 6);
 
-        public void oknoPrzedmiotow(int zaznaczonaOpcja)
-        {
-            Wyswietlanie.okienko("Pancerze do kupienia", 40, 3, 80, 30);
-            Wyswietlanie.pisz("Nazwa", ConsoleColor.White, 43, 6);
-            Wyswietlanie.pisz("Pancerz", ConsoleColor.White, 104, 6);
-            Wyswietlanie.pisz("Cena", ConsoleColor.White, 114, 6);
-
-            // Wyswietlanie.rozdzielacz(28, true, 80, 5);
-            Wyswietlanie.rozdzielacz(28, true, 102, 5);
+            Wyswietlanie.rozdzielacz(28, true, 92, 5);
+            Wyswietlanie.rozdzielacz(28, true, 98, 5);
+            Wyswietlanie.rozdzielacz(28, true, 104, 5);
             Wyswietlanie.rozdzielacz(28, true, 112, 5);
             Wyswietlanie.rozdzielacz(80, false, 40, 7);
 
-            // Wyswietlanie.krzyz(80, 7);
-            Wyswietlanie.krzyz(102, 7);
+            Wyswietlanie.krzyz(92, 7);
+            Wyswietlanie.krzyz(98, 7);
+            Wyswietlanie.krzyz(104, 7);
             Wyswietlanie.krzyz(112, 7);
 
             int y = 8;
-            /*
             ConsoleColor kolor = ConsoleColor.White;
-            for (int i = 0; i < listaCiosow.Length; ++i)
+            for (int i = 0; i < listaCwiczen.Length; ++i)
             {
-                y += 2*i;
                 if(i == zaznaczonaOpcja)
                     kolor = ConsoleColor.Blue;
 
-                Wyswietlanie.pisz(listaCiosow[i].pobierzNazwe(), kolor, 43, y);
-                Wyswietlanie.pisz(listaCiosow[i].pobierzObrazenia().ToString(), kolor, 110, y);
-                Wyswietlanie.pisz(listaCiosow[i].pobierzKoszt().ToString(), kolor, 116, y);
+                Wyswietlanie.pisz(listaCwiczen[i].pobierzNazwe(), kolor, 43, y);
+                Wyswietlanie.pisz($"{listaCwiczen[i].pobierzKoszt()}g", kolor, 94, y);
+                uint[] staty = listaCwiczen[i].pobierzStaty();
+                Wyswietlanie.pisz($"{staty[0]}", kolor, 101, y);
+                Wyswietlanie.pisz($"{staty[1]}", kolor, 108, y);
+                Wyswietlanie.pisz($"{staty[2]}", kolor, 115, y);
 
                 if(i == zaznaczonaOpcja)
                     kolor = ConsoleColor.White;
+
+                y += 2;
             }
-            */
+        }
+
+        public void oknoPrzedmiotow(Przedmiot[] listaPrzedmiotow, int zaznaczonaOpcja)
+        {
+            Wyswietlanie.okienko("Pancerze do kupienia", 40, 3, 80, 30);
+            Wyswietlanie.pisz("Nazwa", ConsoleColor.White, 43, 6);
+            Wyswietlanie.pisz("Pancerz", ConsoleColor.White, 105, 6);
+            Wyswietlanie.pisz("Cena", ConsoleColor.White, 114, 6);
+
+            Wyswietlanie.rozdzielacz(28, true, 104, 5);
+            Wyswietlanie.rozdzielacz(28, true, 112, 5);
+            Wyswietlanie.rozdzielacz(80, false, 40, 7);
+
+            Wyswietlanie.krzyz(104, 7);
+            Wyswietlanie.krzyz(112, 7);
+
+            int y = 8;
+            ConsoleColor kolor = ConsoleColor.White;
+            for (int i = 0; i < listaPrzedmiotow.Length; ++i)
+            {
+                if(i == zaznaczonaOpcja)
+                    kolor = ConsoleColor.Blue;
+
+                Wyswietlanie.pisz(listaPrzedmiotow[i].pobierzNazwe(), kolor, 43, y);
+                Wyswietlanie.pisz(listaPrzedmiotow[i].pobierzPancerz().ToString(), kolor, 108, y);
+                Wyswietlanie.pisz($"{listaPrzedmiotow[i].pobierzCene()}g", kolor, 114, y);
+
+                if(i == zaznaczonaOpcja)
+                    kolor = ConsoleColor.White;
+
+                y += 2;
+            }
         }
 
         public void ekranWalki()
--- src/Program.cs
+++ src/Program.cs
@@ -36,26 +36,25 @@ namespace gra_k
             Wyswietlanie.clrscr();
             interfejs.pasekStatusu(tomek.pobierzStatus(), 2, 200, 3, 230);
 
-            interfejs.ekranDojo(1);
-
-            Cios[] ciosy = new Cios[2] {xd, xd};
-            interfejs.oknoNaukiCiosow(ciosy, 1);
-            //interfejs.oknoPrzedmiotow(2);
-            Wyswietlanie.gotoXY(120, 40);
+            interfejs.ekranDojo(0);
+            
+            Przedmiot xp = new Przedmiot("przedmioty/test.txt");
+            Przedmiot[] przedmioty = new Przedmiot[5] {xp, xp, xp, xp, xp};
+            interfejs.oknoPrzedmiotow(przedmioty, 3);
+            Wyswietlanie.gotoXY(0, 39);
 
             /*
             Console.CursorVisible = false;
             Wyswietlanie.pisz("TEST TEST TEST", false, 10, 1);
             Wyswietlanie.okienko("Tytuł okienka", 2, 2, 30, 20);
-            Wyswietlanie.gotoXY(3, 6);
             Wyswietlanie.pisz("Tutaj coś napisałem", true);
             //Wyswietlanie.gotoXY(3, 7);
             Wyswietlanie.pisz("Tutaj też coś napisałem", false, 3, 7);
             Wyswietlanie.rozdzielacz(18, true, 27, 4);
             Wyswietlanie.pisz("2", true, 29, 6);
             Wyswietlanie.pisz("4", false, 29, 7);
-            Console.CursorVisible = true;
             */
+            Console.CursorVisible = true; 
         }
     }
 }
