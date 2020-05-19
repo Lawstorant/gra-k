57df629d0f4ae7de39900b3bf9009612ba8e7324
Lawstorant <forest10pl@gmail.com>
2020-05-19 19:55:14 +0200

    Wstępnie działające menu Dojo

--- src/Gra.cs
+++ src/Gra.cs
@@ -67,6 +67,12 @@ namespace gra_k
                 {
                     switch(wybor)
                     {
+                        case 0:
+                            this.menuDojo();
+                            instRefresh = true;
+                            nastRefresh = true;
+                            break;
+
                         case 1:
                             przeciwnik = Postac.generujPostac (
                                 (int) this.bohater.pobierzStatus().poziom,
@@ -83,14 +89,91 @@ namespace gra_k
                 else if (wybor > 3)
                     wybor = 0;
 
-            } while ((wybor != 3) || (input.Key != ConsoleKey.Enter));
+            } while (wybor != 3 || input.Key != ConsoleKey.Enter);
         }
 
 
 
-        public void menuDojo()
+        private void menuDojo()
         {
+            int wybor = 0;
+            ConsoleKeyInfo input;
+            var prawyRefresh = true;
+            var statusRefresh = true;
 
+            // decyduje w którym menu się poruszamy:
+            // 0 -> Ćwiczenia
+            // 1 -> Ciosy
+            // 2 -> Przedmioty
+            // 3 -> Ogólne Dojo
+            int ktoreMenu = 3;
+
+            int[] limit = {
+                this.dojo.pobierzCwiczenia().Length,
+                this.dojo.pobierzCiosy().Length,
+                this.dojo.PobierzPrzedmioty().Length,
+                3
+            };
+            
+            do
+            {   
+                // wyświetlam ekran Dojo
+                switch (ktoreMenu)
+                {
+                    case 0:
+                        this.interfejs.oknoCwiczen(this.dojo.pobierzCwiczenia(), wybor);
+                        break;
+                    case 1:
+                        this.interfejs.oknoNaukiCiosow(this.dojo.pobierzCiosy(), wybor);
+                        break;
+                    case 2:
+                        this.interfejs.oknoPrzedmiotow(this.dojo.PobierzPrzedmioty(), wybor);
+                        break;
+                    case 3:
+                        this.interfejs.ekranDojo(wybor);
+                        break;
+                }
+
+                // sprawdzam czy trzeba odświeżyć status naszego bohatera
+                if(statusRefresh)
+                {
+                    this.interfejs.pasekStatusu(this.bohater.pobierzStatus());
+                    statusRefresh = false;
+                }
+
+                // wczytuję wciśnięty klawisz
+                input = Console.ReadKey();
+                
+                // na podstawie klawisza zmieniam wybór,
+                // lub wybieram zaznaczoną opcję
+                if (input.Key == ConsoleKey.UpArrow)
+                    --wybor;
+                else if (input.Key == ConsoleKey.DownArrow)
+                    ++wybor;
+                else if (input.Key == ConsoleKey.Enter)
+                {
+                    if(ktoreMenu == 3 && wybor != 3)
+                    {
+                        ktoreMenu = wybor;
+                        wybor = 0;
+                    }
+                    else
+                    {
+                        
+                        wybor = ktoreMenu;
+                        ktoreMenu = 3;
+                    }
+                }
+
+                // zapętlające się menu
+                if (wybor < 0)
+                    wybor = limit[ktoreMenu];
+                else if (wybor > limit[ktoreMenu])
+                    wybor = 0;
+
+                    // pętla kończy się jeżeli wybór jest równy 3, został wciśnięty enter i
+                    // jesteśmy w ogólnym menu dojo
+            } while (!((wybor == 3) && (input.Key == ConsoleKey.Enter) && (ktoreMenu == 3)));
         }
 
 
